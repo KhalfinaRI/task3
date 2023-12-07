e702f74afeff1a3a5a14e23ea282474eec42a683
Khalfina Regina <xalfina_regina@mail.ru>
2023-12-07 18:39:00 +0300

    Add response on task2

---
+++ Example002/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 2: Задайте массив на 10 целых чисел.  
+// Определите количество чётных чисел в массиве.
+
+void FillArray(int[] array)
+{
+    Console.WriteLine("Введите целые числа для создания массива: ");
+    for (int index = 0; 
+        index < array.Length; 
+        index++)
+    {
+        Console.WriteLine($"Введите [{index}] элемент массива из {array.Length}: ");
+        array[index] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+
+void EvenArray(int[] array)
+{
+    int position = 0;
+
+    for (int index = 0; 
+        index < array.Length; 
+        index++)
+    {
+        if (array[index] % 2 == 0)
+        {
+            position++;
+        }
+    }
+
+    Console.WriteLine($"Количество четных элементов массива = {position}");
+}
+
+int[] array = new int[10];
+FillArray(array);
+EvenArray(array);
+
+
